b2d4ba6a9de75796478e1edb508925169e4b28aa
Roomsie Jones <seangravito@gmail.com>
2021-06-28 17:08:32 -0500

    added the initial API call and tested it through console log

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,23 @@
 // Gotta make variables for city name, apikey, maybe history storage to begin with
-
+var apiKey ="443c6b8b13b68baef62db4e54611e14a"
 
 // After the city name is stored from the search Bar, I'll need to make an API call to get the information based on that city
 
+function callWeather(cityName) {
+    var queryURLCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&apid=" + apiKey;
+    $.ajax({
+        url: queryURLCurrent,
+        method: "GET",
+    })
+        .then(function(response){
+            var cityHeading = response.name;
+            if history.indexOf(cityName) === -1) {
+                history.pushState(cityHeading);
+                localStorage.setItem("history", JSON.stringify(history));
+            }
+            // need to add cityHeading to html and a new var above
+        })
+}
 // I'll need to push that information to my main and mini cards
 
 
