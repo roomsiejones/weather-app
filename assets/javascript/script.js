216fe7391732576ec9f55f2316a25bc542db1f5e
Roomsie Jones <seangravito@gmail.com>
2021-06-28 16:41:03 -0500

    Adding some comments to organize what I need to do

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+// Gotta make variables for city name, apikey, maybe history storage to begin with
+
+
+// After the city name is stored from the search Bar, I'll need to make an API call to get the information based on that city
+
+// I'll need to push that information to my main and mini cards
+
+
+// Make buttons that make that call
+
+// Save city to local storage
+
+
+// Make a call for the saved storage
