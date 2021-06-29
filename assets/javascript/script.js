8f97f7fce6c794180e2327b4a0f4bf2d7623d8d0
Roomsie Jones <seangravito@gmail.com>
2021-06-28 20:11:34 -0500

    Added the pushes to the mini cards, still need to update the html for those to reference into them. API key still producing 401 error...

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,8 @@
-// Gotta make variables for city name, apikey, maybe history storage to begin with
-var apiKey ="443c6b8b13b68baef62db4e54611e14a"
+// Gotta make variables for city name, apiKey, maybe history storage to begin with
+var apiKey ="443c6b8b13b68baef62db4e54611e14a";
+// var apiKey="a9331833ce30bbd78e72624d69688c14"; 
+// var apiKey="f11b180b01f3fa6ea232bdb9d42d03b6";
+// had to create a new testing apiKey b/c initial one was giving 401 error...
 
 var history = JSON.parse(localStorage.getItem("history")) || [];
 
@@ -21,9 +24,9 @@ function callWeather(cityName) {
                 history.pushState(cityHeading);
                 localStorage.setItem("history", JSON.stringify(history));
             }
-            // need to add cityHeading to html and a new var above
+            
 
-            //Need to create the function later that makes the buttons show up
+            
           renderButtons();
           //This will display the city name and locale date.
           $(".currentCityName").text(response.name + " " + moment().format("l"));
@@ -41,7 +44,7 @@ function callWeather(cityName) {
             
             var queryURL_UV =
             "https://api.openweathermap.org/data/2.5/uvi?appid=" +
-            APIKey +
+            apiKey +
             "&lat=" +
             response.coord.lat +
             "&lon=" +
@@ -53,7 +56,75 @@ function callWeather(cityName) {
             
             console.log(response2);
             $("#uvIndex").text("UV Index: " + response2.value);
-            var uvIndex = response2.value;
+            
+
+            var queryURLForecast =
+              "https://api.openweathermap.org/data/2.5/forecast?q=" +
+              cityName +
+              "&units=imperial&appid=" +
+              apiKey;
+            $.ajax({
+              url: queryURLForecast,
+              method: "GET",
+            }).then(function (response3) {
+              console.log(response3);
+
+                 // adds 1-5 number of days to the current to get the days we want and formats it as m/dd/yyyy
+              $("#day1").text(moment().add(1, "days").format("l"));
+              $("#day2").text(moment().add(2, "days").format("l"));
+              $("#day3").text(moment().add(3, "days").format("l"));
+              $("#day4").text(moment().add(4, "days").format("l"));
+              $("#day5").text(moment().add(5, "days").format("l"));
+                //changing icons on the mini-cards
+              var icon1 = response3.list[0].weather[0].icon;
+              var cityIconUrl1 =
+                "https://openweathermap.org/img/w/" + icon1 + ".png";
+              $("#icon1").attr("src", cityIconUrl1);
+              var icon2 = response3.list[1].weather[0].icon;
+              var cityIconUrl2 =
+                "https://openweathermap.org/img/w/" + icon2 + ".png";
+              $("#icon2").attr("src", cityIconUrl2);
+              var icon3 = response3.list[2].weather[0].icon;
+              var cityIconUrl3 =
+                "https://openweathermap.org/img/w/" + icon3 + ".png";
+              $("#icon3").attr("src", cityIconUrl3);
+              var icon4 = response3.list[3].weather[0].icon;
+              var cityIconUrl4 =
+                "https://openweathermap.org/img/w/" + icon4 + ".png";
+              $("#icon4").attr("src", cityIconUrl4);
+              var icon5 = response3.list[4].weather[0].icon;
+              var cityIconUrl5 =
+                "https://openweathermap.org/img/w/" + icon5 + ".png";
+              $("#icon5").attr("src", cityIconUrl5);
+                //Changing temperatures on the mini cards
+              $("#temp1").text("Temp: " + response3.list[0].main.temp + " °F");
+              $("#temp2").text("Temp: " + response3.list[1].main.temp + " °F");
+              $("#temp3").text("Temp: " + response3.list[2].main.temp + " °F");
+              $("#temp4").text("Temp: " + response3.list[3].main.temp + " °F");
+              $("#temp5").text("Temp: " + response3.list[4].main.temp + " °F");
+                //Changing humidity on the mini cards
+              $("#humidity1").text(
+                "Humidity: " + response3.list[0].main.humidity + "%"
+              );
+              $("#humidity2").text(
+                "Humidity: " + response3.list[1].main.humidity + "%"
+              );
+              $("#humidity3").text(
+                "Humidity: " + response3.list[2].main.humidity + "%"
+              );
+              $("#humidity4").text(
+                "Humidity: " + response3.list[3].main.humidity + "%"
+              );
+              $("#humidity5").text(
+                "Humidity: " + response3.list[4].main.humidity + "%"
+              );
+  
+              //Save the latest city to local storage
+              var cityName = $("#city-name").val();
+              console.log(cityName);
+              localStorage.setItem("city", cityName);
+            });
+          });
 
         })
 }
@@ -79,12 +150,3 @@ function renderButtons() {
   $(".list-group").on("click", ".list-group-item", function () {
     callWeather($(this).text());
   });
-// I'll need to push that information to my main and mini cards
-
-
-// Make buttons that make that call
-
-// Save city to local storage
-
-
-// Make a call for the saved storage
